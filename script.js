25114c0c8a0c2c1a649e9e00bd7fddc78397c1b5
norask8 <56714059+norask8@users.noreply.github.com>
2021-01-16 17:33:10 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $('.buttom').mouseover(function(){
-    $('.buttom_active').removeClass('buttom_active');
-  }).mouseout(function(){
-    $('.active').addClass('buttom_active');
-    //$('.buttom_active').css('background-color','#c1c1c1');
-  })
-});
