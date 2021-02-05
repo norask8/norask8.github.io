855444905478152dc1de5a91ed10fa5274d7c757
norask8 <56714059+norask8@users.noreply.github.com>
2021-02-05 10:38:54 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$('.pic #svg_32').delay(3000).fadeIn("slow", function () {
+  //$(this).delay(3000).fadeOut("slow")
+});
+
+$('.pic #svg_33').delay(4000).fadeIn("slow", function () {
+  //$(this).delay(3000).fadeOut("slow");
+});
+
+
+//$(function() {
+  //$('.pic').mouseover(function(){
+    //$('text').fadeIn(3000);
+  //})
+//});
